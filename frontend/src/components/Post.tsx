adf20ca48dd4994101001c6fad1f8d4f1408de55
Joao Cardoso <a50465@alunos.isel.pt>
2024-06-09 16:26:18 +0100

    Post likes, dislikes and views

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -13,13 +13,28 @@ export function Post({post}:{post: PostType}) {
                     }</h2>
                     <span className={"font-inter text-md"}>Posted on {formatDate(date)} at {getTime(date)}</span>
                     </div>
-                    <div id="post-actions" className={"flex flex-col gap-y-3"}>
-                        <button>
-                            <div className={"flex gap-x-2"}>
-                            <span className={"font-inter text-lg"}>{post.likes}</span>
-                            <img className={"w-8 h-8"}></img>
+                    <div id="post-actions" className={"flex select-none flex-col gap-y-10"}>
+
+                            <div className={"flex gap-x-4"}>
+                                <span className={"font-inter text-lg"}>{post.likes}</span>
+                                <button>
+                                <img className={"w-7 h-7 opacity-75 hover:opacity-100"} alt="like"
+                                     src={"https://cdn.jsdelivr.net/gh/CardosoDev04/cdn-test/like.png"}/>
+                                </button>
                             </div>
-                        </button>
+                        <div className={"flex gap-x-4"}>
+                            <span className={"font-inter text-lg"}>{post.dislikes}</span>
+                            <button>
+                                <img className={"w-7 h-7 opacity-75 hover:opacity-100"} alt="dislike"
+                                     src={"https://cdn.jsdelivr.net/gh/CardosoDev04/cdn-test/thumb-down.png"}/>
+                                </button>
+                        </div>
+                        <div className={"flex gap-x-4"}>
+                            <span className={"font-inter text-lg"}>{post.views}</span>
+                                <img className={"w-7 h-7 opacity-75"} alt="eye"
+                                     src={"https://cdn.jsdelivr.net/gh/CardosoDev04/cdn-test/eye.png"}/>
+                        </div>
+
                     </div>
                     <div id="post-content" className={"flex justify-center items-center"}>
                         <span className={"text-2xl"}>{post.content}</span>
